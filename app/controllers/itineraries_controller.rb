5aeb2318c2a132d850f2bdd7d288ec9ad068bb45
kubkowski <jakub.opara@gmail.com>
2014-06-15 01:27:42 +0200

    Added create and update functionality

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -8,4 +8,38 @@ def index
 	@itineraries = Itinerary.all
 end
 
+def new
+	@itinerary = Itinerary.new
+end
+
+def update
+	@itinerary = Itinerary.find(params[:id])
+	if @itinerary.update(itinerary_params)
+		redirect_to action: 'show', id: @itinerary.id
+	else
+		render action: 'edit'
+	end
+end
+
+def edit
+	@itinerary = Itinerary.find(params[:id])
+end
+
+def create
+	@itinerary = Itinerary.new(itinerary_params)
+  if @itinerary.save
+    redirect_to action: 'show', id: @itinerary.id
+  else
+    render action: 'new'
+  end
+end
+
+private
+	
+	def itinerary_params
+		params.require(:itinerary).permit(:location, :travel_on)
+	end
+
+
+
 end
