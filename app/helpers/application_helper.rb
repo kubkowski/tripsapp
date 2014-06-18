dc96e0ef3f657654f5736b205fae60aec8b86129
kubkowski <jakub.opara@gmail.com>
2014-06-18 13:20:12 +0200

    Added delete Itinerary functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
 	def itinerary_row_class(itinerary)
   	'danger' if itinerary.estimated_cost > Itinerary.monthly_budget
 	end
+
+	def sortable(column, title = nil)
+  	title ||= column.titleize
+  	css_class = "current "+sort_direction if column == sort_column
+  	direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+  	link_to title, {sort: column, direction: direction}, {class: css_class}
+	end
+
 end
