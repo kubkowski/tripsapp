566f01ec35de1c9ac695149ecbee2b9c53bbdeb4
kubkowski <jakub.opara@gmail.com>
2014-07-01 19:07:21 +0200

    Added authentication views and signup tests

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,6 +36,16 @@ Rails.application.configure do
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               ENV['GEEKCAMP_EMAIL_DOMAIN'],
+    user_name:            ENV['GEEKCAMP_EMAIL_USERNAME'],
+    password:             ENV['GEEKCAMP_EMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
